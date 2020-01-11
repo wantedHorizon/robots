838bb79f584a3be80df3dfb9e5d13baa5e99db00
WantedHorizon <eliran.amzalag@gmail.com>
2020-01-11 18:30:37 +0200

    added error handler

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 const Card = ({name, email, id}) => {
-    
+  
     return (
         <div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-s'>
             <img alt='robots' src={`https://robohash.org/${id}?size=200x200`} />
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox'
 import Scroll from '../components/Scroll'
+import ErrorBoundry from '../components/ErrorBoundry'
 import './App.css';
 
 class App extends Component {
@@ -30,28 +31,31 @@ class App extends Component {
 
     }
     render() {
-        const { robots , seacfield } = this.state;
+        const { robots, seacfield } = this.state;
         const filterdRobots = robots.filter(robot => {
             return robot.name.toLowerCase().includes(seacfield.toLowerCase());
         });
 
-        return ! robots.length ?  <h1 className="tc">Loading...</h1>:
-    
+        return !robots.length ? <h1 className="tc">Loading...</h1> :
 
 
-             
-                <div className="tc">
-                    <h1 className='f2'>RoboFriends</h1>
-                    <SearchBox searchChange={this.onSearchChange} />
-                    <Scroll>
+
+
+            <div className="tc">
+                <h1 className='f2'>RoboFriends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <Scroll>
+                    <ErrorBoundry>
                         <CardList robots={filterdRobots} />
-                    </Scroll>
 
-                </div>
+                    </ErrorBoundry>
+                </Scroll>
+
+            </div>
 
 
-            
-        
+
+
 
     }
 
