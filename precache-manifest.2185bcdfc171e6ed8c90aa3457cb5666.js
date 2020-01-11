ebd88347dea216edee52fe0cb17182fb556795d9
WantedHorizon <eliran.amzalag@gmail.com>
2020-01-11 18:58:29 +0200

    Updates

---
+++ precache-manifest.2185bcdfc171e6ed8c90aa3457cb5666.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da24e52e53fae592e041fc370ad20fc8",
+    "url": "/robots/index.html"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/robots/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f17b7172dac9956aec1b",
+    "url": "/robots/static/css/main.d4f4605f.chunk.css"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/robots/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/robots/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f17b7172dac9956aec1b",
+    "url": "/robots/static/js/main.2b3975c1.chunk.js"
+  },
+  {
+    "revision": "904ae3b3b0a9999ec656",
+    "url": "/robots/static/js/runtime-main.5fd8c5d6.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robots/static/media/SEGA.12a3db8b.woff"
+  }
+]);
