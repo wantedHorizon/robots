feda824255dd9c91aaef032f8702dcb4ebc31e49
WantedHorizon <eliran.amzalag@gmail.com>
2020-01-11 17:48:17 +0200

    files added

---
+++ src/containers/App.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox'
+import Scroll from '../components/Scroll'
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            robots: [],
+            seacfield: ''
+        };
+    }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(res => res.json())
+            .then(users => this.setState({ robots: users }));
+
+
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ seacfield: event.target.value })
+
+
+
+        // this.state.robots= filterdRobots;
+
+    }
+    render() {
+        const { robots , seacfield } = this.state;
+        const filterdRobots = robots.filter(robot => {
+            return robot.name.toLowerCase().includes(seacfield.toLowerCase());
+        });
+
+        return ! robots.length ?  <h1 className="tc">Loading...</h1>:
+    
+
+
+             
+                <div className="tc">
+                    <h1 className='f2'>RoboFriends</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <Scroll>
+                        <CardList robots={filterdRobots} />
+                    </Scroll>
+
+                </div>
+
+
+            
+        
+
+    }
+
+
+}
+
+export default App;
